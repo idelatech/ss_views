0ec7c0da58151dcd3790dd725f104f61789e0573
Phillip Hall <phillip_hall@homedepot.com>
2019-03-25 23:33:26 -0400

    more user profile page scaffolding, adding scrollable cardlist and finding icons

--- src/Main/App.view.logic.js
+++ src/Main/App.view.logic.js
@@ -3,6 +3,6 @@ import App from './App.view.js'
 
 export default class AppLogic extends React.Component {
   render() {
-    return <App {...this.props} />
+    return <App isVideoSelected={true} {...this.props} />
   }
 }
